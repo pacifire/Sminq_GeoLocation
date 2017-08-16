dbbec89493132f8a70d36e94c8f2a9662bd6ca28
Pawan Shahani <pawan.shahani@gmail.com>
2017-08-16 17:29:13 +0530

    -- GeoFencing created with Raidus, LAtitude & Longitude.

--- app/src/main/java/sminq/com/geolocation_test/MainActivity.java
+++ app/src/main/java/sminq/com/geolocation_test/MainActivity.java
@@ -1,6 +1,7 @@
 package sminq.com.geolocation_test;
 
 import android.Manifest;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -15,8 +16,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 /**
  * Created by Pawan on 16/08/17.
@@ -35,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
     private final double longitude = 40;//This is the Longitude of the GeoFencing.
     private GeofencingClient mGeoFencingClient;
     private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+    private final String GEO_FENCE_KEY = "SMINQ_OFFICE_LOCATION";
+
+
+    private GeofencingRequest geoFencingRequst;
+    private PendingIntent geoFencingIntent;
 
 
 
@@ -175,7 +186,46 @@ public class MainActivity extends AppCompatActivity {
      * This method is called, when permissions are permitted.
      * **/
     private void addGeofences() {
-        Log.d(TAG, "addGeofences: ");
+
+        if(mGeoFencingClient != null) {
+
+            //Creating GeoFencing Request.....
+            GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+            builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);/** The INITIAL_TRIGGER_ENTER flag indicates that geofencing service should trigger a
+                                                                                    GEOFENCE_TRANSITION_ENTER notification when the geofence is added and if the device
+                                                                                    is already inside that geofence.**/
+
+
+            Calendar geoFenceExpiryCalendar = Calendar.getInstance();
+            geoFenceExpiryCalendar.add(Calendar.HOUR, 24);//We will add 24 Hrs, in the Calendar, i,e, GeFencing will expire after 2 Hrs from current Time.
+
+            Geofence geoFence = new Geofence.Builder().setRequestId(GEO_FENCE_KEY)
+                                    .setCircularRegion(latitude, longitude, radius)
+                                    .setExpirationDuration(geoFenceExpiryCalendar.getTimeInMillis())
+                                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_EXIT)
+                                    .build();
+
+
+            // Add the geofences to be monitored by geofencing service.
+            ArrayList<Geofence> geofencesAl = new ArrayList<>();
+            geofencesAl.add(geoFence);
+            builder.addGeofences(geofencesAl);
+            geoFencingRequst = builder.build();
+
+
+            //Creating Pending Intent for GeoFencing....
+            Intent intent = new Intent(this, GeofenceTransitionIntentService.class);
+            // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
+            // addGeofences() and removeGeofences().
+            geoFencingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+
+            if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                mGeoFencingClient.addGeofences(geoFencingRequst, geoFencingIntent);
+            else
+                Log.w(TAG, "Geo Fence not added, bcoz of permission issues !");
+        }//if(mGeoFencingClient != null) closes here....
     }//addGeofences closes here....
 
 
